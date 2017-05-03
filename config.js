6506109c990b897db68676055618ba365dc7dd40
Josep Roman Martinez Riera <pepinho@gmail.com>
2017-05-03 13:06:32 +0200

    Set Up the mongoDb conf file

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
 module.exports = {
-    'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/test2'
+    'mongoUrl' : 'kandy:dami33@ec2-35-158-94-35.eu-central-1.compute.amazonaws.com:27017/Go'
 }
