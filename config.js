5d3c53fa36f6dcadc917078971f3b40e5b049550
Josep Roman Martinez Riera <pepinho@gmail.com>
2017-05-04 10:51:26 +0200

    jquery index client update

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'kandy:dami33@localhost:27017/Go'
+    'mongoUrl' : 'kandy:dami33@ec2-35-158-94-35.eu-central-1.compute.amazonaws.com:27017/Go'
 }
